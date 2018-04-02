b116e0d7d90974dc2b169d6839e7e28919d96491
Stepan <rainbowprodder@gmail.com>
2018-04-02 18:57:32 +0700

    другой контрол #26

--- src/index.js
+++ src/index.js
@@ -22,36 +22,34 @@ $(document).ready(function() {
     from: 10,
     to: 50,
     onCreate: (e, d) => {
-      from.html(`from: ${d.from}`);
-      to.html(`to: ${d.to}`);
       create.addClass('active');
       clearTimeout(timeId);
       timeId = setTimeout(remove, 200);
     },
     onStart: (e, d) => {
-      from.html(`from: ${d.from}`);
-      to.html(`to: ${d.to}`);
+      from.val(d.from);
+      to.val(d.to);
       start.addClass('active');
       clearTimeout(timeId);
       timeId = setTimeout(remove, 200);
     },
     onSlide: (e, d) => {
-      from.html(`from: ${d.from}`);
-      to.html(`to: ${d.to}`);
+      from.val(d.from);
+      to.val(d.to);
       slide.addClass('active');
       clearTimeout(timeId);
       timeId = setTimeout(remove, 200);
     },
     onEnd: (e, d) => {
-      from.html(`from: ${d.from}`);
-      to.html(`to: ${d.to}`);
+      from.val(d.from);
+      to.val(d.to);
       end.addClass('active');
       clearTimeout(timeId);
       timeId = setTimeout(remove, 200);
     },
     onUpdate: (e, d) => {
-      from.html(`from: ${d.from}`);
-      to.html(`to: ${d.to}`);
+      from.val(d.from);
+      to.val(d.to);
       update.addClass('active');
       clearTimeout(timeId);
       timeId = setTimeout(remove, 200);
